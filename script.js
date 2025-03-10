3cd60e39d64d346a99593f02b4f23a2464e0df97
Braiu <bryanduarte3714@gmail.com>
2025-03-09 21:23:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,90 @@
+var canvas;
+var stage;
+var container;
+var captureContainers;
+var captureIndex;
+
+function init() {
+  // cria o stage e aplica no canvas
+  canvas = document.getElementById("testCanvas");
+  stage = new createjs.Stage(canvas);
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
+  var w = canvas.width;
+  var h = canvas.height;
+
+  container = new createjs.Container();
+  stage.addChild(container);
+
+  captureContainers = [];
+  captureIndex = 0;
+
+  // cria os shapes, e controla sua velocidade e angulos:
+
+		for (var i = 0; i < 100; i++) {
+			var heart = new createjs.Shape();
+			heart.graphics.beginFill('#58a5e8'); //defini cor do coração
+			heart.graphics.moveTo(0, -12).curveTo(1, -20, 8, -20).curveTo(16, -20, 16, -10).curveTo(16, 0, 0, 12);
+			heart.graphics.curveTo(-16, 0, -16, -10).curveTo(-16, -20, -8, -20).curveTo(-1, -20, 0, -12);
+			heart.y = -100;
+
+			container.addChild(heart);
+		}
+
+  var text = new createjs.Text("Feliz dia Meu Bem\nVocê é tão incrivel e muito especial para mim\nEu amo estar com você!", "bold 24px Arial", "#357");
+  text.textAlign = "center";
+  text.x = w / 2;
+  text.y = h / 2 - text.getMeasuredLineHeight();
+  stage.addChild(text);
+
+  for (i = 30; i < 100; i++) {
+    var captureContainer = new createjs.Container();
+    captureContainer.cache(0, 0, w, h);
+    captureContainers.push(captureContainer);
+  }
+
+  // limita o stage
+  createjs.Ticker.timingMode = createjs.Ticker.RAF;
+  createjs.Ticker.on("tick", tick);
+}
+
+function tick(event) {
+  var w = canvas.width;
+  var h = canvas.height;
+  var l = container.numChildren;
+
+  captureIndex = (captureIndex + 1) % captureContainers.length;
+  stage.removeChildAt(0);
+  var captureContainer = captureContainers[captureIndex];
+  stage.addChildAt(captureContainer, 0);
+  captureContainer.addChild(container);
+
+  // interação com todas as crianças e movimentos
+		for (var i = 0; i < l; i++) {
+			var heart = container.getChildAt(i);
+			if (heart.y < -50) {
+				heart._x = Math.random() * w;
+				heart.y = h * (1 + Math.random()) + 50;
+				heart.perX = (1 + Math.random() * 2) * h;
+				heart.offX = Math.random() * h;
+				heart.ampX = heart.perX * 0.1 * (0.15 + Math.random());
+				heart.velY = -Math.random() * 2 - 1;
+				heart.scale = Math.random() * 2 + 1;
+				heart._rotation = Math.random() * 40 - 20;
+				heart.alpha = Math.random() * 0.75 + 0.05;
+				heart.compositeOperation = Math.random() < 0.33 ? "lighter" : "source-over";
+			}
+			var int = (heart.offX + heart.y) / heart.perX * Math.PI * 2;
+			heart.y += heart.velY * heart.scaleX / 2;
+			heart.x = heart._x + Math.cos(int) * heart.ampX;
+			heart.rotation = heart._rotation + Math.sin(int) * 30;
+		}
+
+  captureContainer.updateCache("source-over");
+
+  // atualiza e mostra as atualizações no stage
+  stage.update(event);
+}
+
+init();
